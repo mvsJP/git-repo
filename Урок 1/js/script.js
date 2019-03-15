fff82cddcd63498e8df221c294b15103362f4b31
mvs <mvss@yandex.ru>
2019-03-15 16:39:12 +0300

    Задание к уроку №1

---
+++ Урок 1/js/script.js
@@ -0,0 +1,27 @@
+
+// Задание №1
+
+/*let Tc = parseInt(prompt("Укажите температуру в градусах по Цельсию: "));
+let Tf = (9 / 5) * Tc + 32;
+
+alert("Температура в градусах по Фаренгейту составляет: " + Tf);
+*/
+
+// Задание №2
+
+let a = 14 * "ckjdj";
+console.log(typeof(a));
+console.log(a);
+
+// Задание №3
+
+let admin;
+let name = "Василий";
+admin = name;
+console.log(admin);
+
+// Задание №4
+
+a = 1000 + "108";
+console.log(a);  // 1000108
+
